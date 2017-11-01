67e74c30bf7832a140b9699de269d4259e72ed2a
Laila Ali <ms.laila.ali@outlook.com>
2017-11-01 01:52:36 -0400

    worked on lab2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,79 @@
+//VARIABLES
+
+let tableNum = $(this).text();
+
+
+
+//Cursor changes
+
+$('.avail, #popupBtn, #reserveBtn').css( 'cursor', 'pointer' );
+
 
 
 //When 'available' table is clicked, the form pops up
-
-$('.avail, #popupBtn, #saveBtn').css( 'cursor', 'pointer' );
-
-$ (".table, avail").on("click", function(e){
-    $("#hideForm").show().delay(1200).fadeIn(800);
+$ (".avail").on("click", function(e){
+    $("#hideForm").fadeIn(800);
 });
 
-//Form can be closed by hitting the 'x' button
 
+//Selected table is shown on form.
+$(".table").on("click", function(e){
+    var selectedTable = $(this).text();
+    $("#selectedTable").text(" " + selectedTable);
+    $(this).addClass('selected');
+});
+
+
+
+//When Reserve Btn is clicked, remove class of 'selected' & 'avail'.
+//Add class of 'reserved'
+$(".reservingBtn").on("click", function(e){ 
+    $("#hideForm").fadeOut(800);
+    $(".selected").addClass("reserved").removeClass("selected", "avail")
+});
+
+
+//When Reserve Button is clicked, take input value and show on hover of that reserved table.
+
+ 
+function retrieveData(){
+    var name = $(".inputGuestName").val;
+    var partySize = $(".inputGuestPhone").val;
+
+    $(".hoverBox", this).append(name);
+    $(".hoverBox", ".reserved").append(partySize);
+};
+retrieveData();
+
+
+//Change reserve table to be unclickable.
+$(".reserved").hover(function() {
+	$(".hoverBox").show();
+ 	});
+
+
+
+
+
+
+
+
+
+
+/* When 'x' is clicked, remove 'reserved' class.
+And close the form */
 $("#popupBtn").on("click", function(e){
-    $("#hideForm").hide().delay(1200).fadeOut(800);
-});
-
-//Available tables change colors and cursor changes.
-$(".avail").hover(function(){
-    $(this).css("background-color", "#333333");
-    }, function(){
-    $(this).css("background-color", "#d3d3d3");
-});
+    $(".selected").removeClass("selected");
+    $("#hideForm").fadeOut(800);
+})
 
 
 
-//NEED HELP W/CODE BELOW
 
 
-//Selected table is shown on New Res. Table Number
-$(".table, avail").on("click", function(e){
-    $(this).append(this.text);
-});
-
-//Table changes to 'Reserved' when form is Saved.
-$(".avail").click(function(){
-    $.change($(this).text()).$("#tableNum");
-});
 
 
-//Cursor changes on 'res' tables.
-$('#res').css('cursor', 'value');
+    
 
 
 
